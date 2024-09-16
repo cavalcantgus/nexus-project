c37ba0208490db7e9b8b58d2dc50c91fe0d51cdd
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-09-16 16:51:57 -0300

    Modifying App CSS

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 </style>
