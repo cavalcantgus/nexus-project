90fb57e7afc4639d6f9454cdca05716abb1375f5
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-09-25 15:22:59 -0300

    Styling FormDialog

--- src/components/FormDialog.vue
+++ src/components/FormDialog.vue
@@ -1,18 +1,20 @@
 <template>
-    <v-dialog>
-        <v-card>
+    <v-dialog v-if="localVisible" v-model="localVisible" width="500px">
+        <v-card class="custom-card">
             <v-card-title>
                 <span class="card-title">Formate seu arquivo</span>
-                <v-form>
-                    <v-combobox7
-                    label="Escolha o tipo de relatório"
+                <v-form ref="form">
+                    <v-combobox
+                    label="Escolha o tipo de relatório" class="input-field"
                     :items="list_report"
                     item-title="type"
                     item-value="id"
-                    v-model="report_type">
-                    </v-combobox7>
-                    <v-input-file
-                    label="Upload your file"></v-input-file>
+                    v-model="localreport_type">
+                    </v-combobox>
+                    <v-file-input
+                    label="Upload your file" class="input-field"
+                    accept=".xlsx, .xls"
+                    v-model="localfile_name"></v-file-input>
                     <v-card-actions>
                         <v-btn class="cancel-button" @click="close">Cancelar</v-btn>
                         <v-btn class="format-button" @click="format">Formatar</v-btn>
@@ -27,7 +29,9 @@
 export default {
     name: 'FormDialog',
     props:  {
-        visible: Boolean
+        visible: Boolean,
+        report_type: [String, Number, Object],
+        file_name: File
     },
     data() {
         return {
@@ -39,7 +43,8 @@ export default {
                 {"id": 4, "type": "Marketing"},
                 {"id": 5, "type": "Obras"},
                 ],
-            report_type: null
+            localreport_type: this.report_type || null,
+            localfile_name: this.file_name || null
         }
     },
     watch: {
@@ -49,14 +54,62 @@ export default {
             }
         }
     },
-    methods: {
-        close() {
-            this.$emit('cancel')
-        },
-        
-        formar() {
-            this.$emit('format')
+   methods: {
+    format() {
+        if(this.localfile_name && this.localreport_type){
+            this.$emit('format', this.localfile_name, this.localreport_type)
         }
     }
+   }
+
 }
 </script>
+
+<style scoped>
+
+:deep(.custom-card ){
+    border: 3px solid #222;
+    background-color: rgb(240, 238, 238);
+}
+
+.card-title {
+    text-align: center;
+    color: rgb(5, 6, 65);
+    width: 100%;
+    display: block;
+    margin-bottom: 16px;
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.input-field {
+    color: rgb(5, 6, 65);
+}
+
+:deep(.format-button) {
+    background-color: rgb(5, 6, 65);
+    color: aliceblue;
+    margin-left: 10px;
+    font-weight: bold;
+    font-size: 16px;
+    cursor: pointer;
+    transition: all .2s ease-in-out;
+}
+
+:deep(.format-button:hover) {
+    transform: scale(1.1);
+}
+
+:deep(.cancel-button) {
+    background-color: rgb(184, 18, 18);
+    color: aliceblue;
+    font-weight: bolder;
+    font-size: 16px;
+    cursor: pointer;
+    transition: all .2s ease-in-out;
+}
+
+:deep(.cancel-button:hover) {
+    transform: scale(1.1);
+}
+</style>
--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -2,16 +2,45 @@
     <BannerView/>
     <div>
         <h1>HOME</h1>
+        <v-btn color="primary" @click="showForm">Formatar planilha</v-btn>
+        <FormDialog
+        :visible="formVisible"
+        :report_type="type"
+        :file_name="file"
+        @format="formatSheet"
+        @cancel="cancelFormat"/>
     </div>
 </template>
 
 <script>
 import BannerView from './BannerView.vue'
+import FormDialog from './FormDialog.vue'
 
 export default {
     name: 'HomeView',
     components: {
-        BannerView
+        BannerView,
+        FormDialog
+    },
+    data() {
+        return {
+            formVisible: false,
+            file: null,
+            type: null
+        }
+    },
+    methods: {
+        showForm() {
+            this.formVisible = true
+        },
+        formatSheet(file_name, report_type) {
+            this.file = file_name
+            this.type = report_type
+            console.log(`${this.file.name}, ${this.type.type}`)
+        },
+        cancelFormat() {
+            this.formVisible = false
+        }
     }
 }
 </script>
