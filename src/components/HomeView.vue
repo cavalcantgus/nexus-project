8974c7e68974b5a12059ff044c003004e9c81ed9
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-09-25 16:06:44 -0300

    arranging components in HomeView

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,8 +1,14 @@
 <template>
     <BannerView/>
-    <div>
-        <h1>HOME</h1>
-        <v-btn color="primary" @click="showForm">Formatar planilha</v-btn>
+    <div class="main-container">
+        <h1>Formatar uma planilha nunca foi tão fácil</h1>
+        <div class="paragraph">
+            <p>Para formatar a sua planilha, é bem simples. Clique no botão <span>Formatar planilha</span>
+            escolha o tipo de relatório você deseja formatar, em seguida, escolha seu arquivo (Lembre-se, precisa ser um arquivo .xlsx).
+            e clique em Formatar. Agora é só aguardar que seu arquivo será formatado e logo estará pronto para você</p>
+        </div>
+        <img src="\img\setas-direitas.png" alt="" class="seta">
+        <v-btn @click="showForm" class="btn-format">Formatar planilha</v-btn>
         <FormDialog
         :visible="formVisible"
         :report_type="type"
@@ -44,3 +50,60 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+.main-container {
+    background-color: rgb(71, 70, 146);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+h1 {
+    color: rgb(231, 234, 236);
+}
+
+.paragraph {
+    background-color: #f8be00;
+    width: 40%;
+    align-self: flex-start;
+    margin-top: 100px;
+    margin-left: 50px;
+    padding: 30px;
+    border-bottom: 4px solid rgba(5, 6, 65, 0.555);
+    border-radius: 10px;
+    display: flex;
+    align-items: flex-start;
+    transition: all .2s ease-in-out;
+    margin-bottom: 20px;
+}
+
+.paragraph:hover {
+    transform: scale(1.1);
+}
+
+p {
+    font-size: 16px;
+    color: #222;
+    font-weight: bold;
+}
+
+.btn-format {
+    background-color: rgb(5, 6, 65);
+    color: aliceblue;
+    font-weight: bold;
+    height: 50px;
+    margin-top: -50px;
+    align-self: flex-end;
+    margin-right: 50px;
+    margin-bottom: 20px;
+}
+
+.seta {
+    width: 100px;
+    align-self: center;
+    margin-top: -100px;
+    margin-left: 200px;
+}
+</style>
